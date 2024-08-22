9db9052b3e7956f01b4afeefc25d8275060295e8
rehankhan88 <rehan9732088@gmail.com>
2024-08-22 02:50:09 -0700

    I have add prompt

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+# import tkinter as tk
+# import customtkinter as ctk 
 
-app = Flask(__name__)
+# from PIL import ImageTk 
+# # from authtoken import auth_token
 
-@app.route('/')
-def index():
-    return 'Rehan khan flask'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81)
+# import torch 
+# from torch import autocast 
+# from diffusers import StableDiffusionPipeline
+
+# # create app
+# app = tk.Tk()
+# app.geometry("532x622")
+# app.title("Stable Bud")
+# ctk.set_appearance_mode("dark")
+
+# prompt = ctk.CTkEntry(height=40, width=512, text_font=("Arial", 20), text_color="black", fg_color="white")
+# prompt.place(x=10, y=10)
+
+
+
+# app.mainloop()
+
+
+
+import tkinter as tk
+import customtkinter as ctk 
+
+from PIL import ImageTk 
+# from authtoken import auth_token
+
+import torch 
+from torch import autocast 
+from diffusers import StableDiffusionPipeline
+
+# create app
+app = tk.Tk()
+app.geometry("532x622")
+app.title("Stable Bud")
+ctk.set_appearance_mode("dark")
+
+# create an entry widget with the master argument
+prompt = ctk.CTkEntry(master=app, height=40, width=512, font=("Arial", 20), fg_color="white", text_color="black")
+
+
+# place the widget in the window (use pack, grid, or place)
+prompt.pack(pady=20)
+
+app.mainloop()
