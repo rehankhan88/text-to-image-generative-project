f5f9949a08c284532487960b68315b93e5ba208a
rehankhan88 <rehan9732088@gmail.com>
2024-08-22 03:07:23 -0700

    I have add button

--- app.py
+++ app.py
@@ -1,29 +1,3 @@
-# import tkinter as tk
-# import customtkinter as ctk 
-
-# from PIL import ImageTk 
-# # from authtoken import auth_token
-
-
-# import torch 
-# from torch import autocast 
-# from diffusers import StableDiffusionPipeline
-
-# # create app
-# app = tk.Tk()
-# app.geometry("532x622")
-# app.title("Stable Bud")
-# ctk.set_appearance_mode("dark")
-
-# prompt = ctk.CTkEntry(height=40, width=512, text_font=("Arial", 20), text_color="black", fg_color="white")
-# prompt.place(x=10, y=10)
-
-
-
-# app.mainloop()
-
-
-
 import tkinter as tk
 import customtkinter as ctk 
 
@@ -46,5 +20,11 @@ prompt = ctk.CTkEntry(master=app, height=40, width=512, font=("Arial", 20), fg_c
 
 # place the widget in the window (use pack, grid, or place)
 prompt.pack(pady=20)
+
+Imain = ctk.CTkLabel(master=app, width=512, height=512)
+Imain.place(x=10, y=110)
+
+trigger = ctk.CTkButton(master=app, height=40, width=120, font=("Arial", 20), fg_color="blue", text_color="white")
+trigger.place(x=206, y=70)
 
 app.mainloop()
