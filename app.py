b6bb763d55e0679a5ce9eceaa7e87d644c378283
rehankhan88 <rehan9732088@gmail.com>
2024-08-22 03:45:26 -0700

    some functions add

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import customtkinter as ctk 
 
 from PIL import ImageTk 
-# from authtoken import auth_token
+from authtoken import auth_token
 
 import torch 
 from torch import autocast 
@@ -24,7 +24,21 @@ prompt.pack(pady=20)
 Imain = ctk.CTkLabel(master=app, width=512, height=512)
 Imain.place(x=10, y=110)
 
-trigger = ctk.CTkButton(master=app, height=40, width=120, font=("Arial", 20), fg_color="blue", text_color="white")
-trigger.place(x=206, y=70)
+modelid= "CompVis/stable-diffusion-v1-4"
+device = "cuda"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtypes=torch.float16, use_auth_token=auth_token)
+pipe.to(device)
 
+
+def generate():
+    with autocast(device):
+        image = pipe(prompt.get(), guidance_scale=8.5)["sample"][0]
+    img = ImageTk.PhotoImage(image)
+    img.save("generatedimage.png")
+    Imain.confgure(image=img)
+
+
+trigger = ctk.CTkButton(master=app, height=40, width=120, font=("Arial", 20), fg_color="blue", text_color="white", command=generate)
+trigger.configure(text= "Generate")
+trigger.place(x=206, y=70)
 app.mainloop()
